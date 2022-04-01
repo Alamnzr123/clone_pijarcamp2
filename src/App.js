eff3951489394a728a86eea2067f8ae7899df44d
Rahmad Alamsyah Nazaruddin <54619997+Alamnzr123@users.noreply.github.com>
2022-04-01 16:35:32 +0700

    Clone_pijarcamp2

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import Nav from './Components/Nav';
+import Home from './Components/Home';
+
+function App() {
+  return (
+    <div>
+    <Nav />
+    <Home />
+  </div>
+  );
+}
+
+export default App;
